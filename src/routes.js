cdee770e5c0060a64a847974232f8ec2393704ee
Pablo Muguerza <pmuguer@gmail.com>
2019-04-27 19:53:25 -0300

    Otro borrador de uso de react-route

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ var React = require("react");
 var Router = require("react-router");
 var DefaultRoute = Router.DefaultRoute;
 var App = require("./components/app");
+var Authors = require("./components/authors/authorPage");
 
 var Route = Router.Route;
 
 var routes = (
-    <Route name="app" path="/" handler={App} >
+    <Route name="app" path="/" handler={Authors} >
+        <Route name="authors" handler={Authors} />
     </Route>
 );
 
