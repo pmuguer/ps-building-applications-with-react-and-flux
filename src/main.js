30f936a9fd5b7cfb7657c85ae7bc4770a22abb05
Pablo Muguerza <pmuguer@gmail.com>
2019-04-27 00:52:31 -0300

    Cap 4, Demo: Simple Routing

--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
-// Bootrap espera que jquery esté en el global namespace
-$ = jQuery = require('jquery');
+"use strict";
+var $ = require('jquery');
+var jQuery = $;
 var React = require('react');
 var Home = require('./components/homePage');
+var About = require('./components/about/homePage');
 
-React.render(<Home />, document.getElementById("app"));
+class App extends React.Component {
+    render() {
+        var Child;
+        switch (this.props.route) {
+            case 'about': Child = About; break;
+            default: Child = Home;
+        }
+        return (
+            <div>
+                <Child />
+            </div>
+        );
+    }
+}
+
+function render() {
+    var route = window.location.hash.substr(1);
+    React.render(<App route={route} />, document.getElementById("app"));
+}
+
+window.addEventListener('hashchange', render);
+render();
