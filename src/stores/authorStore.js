c6d39fe8ee7cc070e246728d43ed0305a5dc6f4f
Pablo Muguerza <pmuguer@gmail.com>
2019-04-29 00:09:44 -0300

    Cap 10, Stores: private storage

--- src/stores/authorStore.js
+++ src/stores/authorStore.js
@@ -6,7 +6,10 @@ var ActionTypes = require('../constants/actionTypes');
 // broadcast de eventos
 var EventEmitter = require('events').EventEmitter;
 var assign = require("object-assign");
+var _ = require('lodash');
 var CHANGE_EVENT = 'change';
+
+var _authors = [];
 
 // "Mixin": prototype + el objeto que se define
 var AuthorStore = assign({}, EventEmitter.prototype, {
@@ -21,13 +24,23 @@ var AuthorStore = assign({}, EventEmitter.prototype, {
     emitChange: function() {
         this.emit(CHANGE_EVENT);
     }
+
+    getAllAuthors: function() {
+        return _authors;
+    }
+
+    getAuthorById: function(id) {
+        return _.find(_authors, {id: id});
+    }
 });
 
-/*
+
 Dispatcher.register(function(action) {
     switch(action.ActionTypes) {
-
+        case ActionTypes.CREATE_AUTHOR:
+            _authors.push(action.author);
+            AuthorStore.emitChange();
     }
-}); */
+});
 
 module.exports = AuthorStore;
