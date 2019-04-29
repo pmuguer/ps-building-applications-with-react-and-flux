25400ee64d049a379aafe343036dcb228a0b21a0
Pablo Muguerza <pmuguer@gmail.com>
2019-04-29 20:22:51 -0300

    Cap 10, Delete Author via Flux

--- src/components/authors/authorList.js
+++ src/components/authors/authorList.js
@@ -13,15 +13,15 @@ var AuthorList = React.createClass({
     },
 
     render: function() {
-        function createTableRow(author) {
+        var createTableRow = function(author) {
             return (
                 <tr key={author.id}>
-                    <td><a href="#" onClick={this.deleteAuthor.bind(this.author.id)}>Delete</a></td>
+                    <td><a href="#" onClick={this.deleteAut.bind(this, author.id)}>Delete</a></td>
                     <td><Link to="manageAuthor" params={{id: author.id}}>{author.id}</Link></td>
                     <td>{author.firstName} {author.lastName}</td>
                 </tr>
             );
-        }
+        };
         return (
             <div>
                 <table className="table">
